6636ecb987c70dbf26c719588013af681069864f
puffball <puffballdesigns@aol.com>
2022-06-30 11:57:49 -0400

    Initial Commit

---
+++ BTBubbleLevel/BTBubbleLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTBubbleLevelApp.swift
+//  BTBubbleLevel
+//
+//  Created by Matthew Tooley on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct BTBubbleLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BTBubbleLevel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BTBubbleLevel
+//
+//  Created by Matthew Tooley on 6/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
